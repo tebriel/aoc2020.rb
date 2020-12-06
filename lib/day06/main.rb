452da60872235c44619a8e9948c6e01a90200d7d
Chris M <tebriel@github.com>
2020-12-06 05:28:41 +0000

    Tests and the very ugly solution

--- lib/day06/main.rb
+++ lib/day06/main.rb
@@ -1,8 +1,63 @@
 # frozen_string_literal: true
 
-if $PROGRAM_NAME == __FILE__
-  lines = []
-  File.foreach('inputs/day06.txt') do |line|
-    lines.append(line.chomp)
+require 'set'
+
+# Handle any question that anyone said yes to
+class GroupResponses
+  def initialize
+    @answers = Set.new
   end
+
+  def log_answers(resps)
+    resps.split('').each do |resp|
+      @answers.add(resp)
+    end
+  end
+
+  def yeses
+    @answers.count
+  end
+end
+
+# Handle the yeses that everyone had
+class GroupResponses2
+  def initialize
+    @answers = {}
+    @group_count = 0
+  end
+
+  def log_answers(resps)
+    @group_count += 1
+    resps.split('').each do |resp|
+      @answers[resp] ||= 0
+      @answers[resp] += 1
+    end
+  end
+
+  def yeses
+    (@answers.filter { |_key, value| value == @group_count }).count
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  groups = [GroupResponses.new]
+  File.foreach('inputs/day06.txt') do |line|
+    groups.append(GroupResponses.new) if line.chomp.empty?
+    group = groups.last
+    group.log_answers(line.chomp)
+  end
+
+  puts(groups.sum(&:yeses))
+
+  groups = [GroupResponses2.new]
+  File.foreach('inputs/day06.txt') do |line|
+    if line.chomp.empty?
+      groups.append(GroupResponses2.new)
+      next
+    end
+    group = groups.last
+    group.log_answers(line.chomp)
+  end
+
+  puts(groups.sum(&:yeses))
 end
--- test/day06/main_test.rb
+++ test/day06/main_test.rb
@@ -3,8 +3,88 @@
 require 'minitest/autorun'
 require_relative '../../lib/day06/main'
 
-describe '#' do
-  it 'blank test' do
-    assert true
+describe 'GroupResponses' do
+  describe '#log_answers' do
+    def setup
+      @resps = GroupResponses.new
+    end
+    it 'counts a solo group' do
+      @resps.log_answers('abc')
+
+      assert 3, @resps.yeses
+    end
+
+    it 'counts a multi group' do
+      @resps.log_answers('a')
+      @resps.log_answers('b')
+      @resps.log_answers('c')
+
+      assert 3, @resps.yeses
+    end
+
+    it 'counts a multi group with repeat answers' do
+      @resps.log_answers('ab')
+      @resps.log_answers('ac')
+
+      assert 2, @resps.yeses
+    end
+
+    it 'counts a multi group with very repeated answers' do
+      @resps.log_answers('a')
+      @resps.log_answers('a')
+      @resps.log_answers('a')
+      @resps.log_answers('a')
+
+      assert 1, @resps.yeses
+    end
+
+    it 'counts a solo group with a single answer' do
+      @resps.log_answers('b')
+
+      assert 1, @resps.yeses
+    end
+  end
+end
+
+describe 'GroupResponses2' do
+  describe '#log_answers' do
+    def setup
+      @resps = GroupResponses2.new
+    end
+    it 'counts a solo group' do
+      @resps.log_answers('abc')
+
+      assert 3, @resps.yeses
+    end
+
+    it 'counts a multi group' do
+      @resps.log_answers('a')
+      @resps.log_answers('b')
+      @resps.log_answers('c')
+
+      assert 0, @resps.yeses
+    end
+
+    it 'counts a multi group with repeat answers' do
+      @resps.log_answers('ab')
+      @resps.log_answers('ac')
+
+      assert 1, @resps.yeses
+    end
+
+    it 'counts a multi group with very repeated answers' do
+      @resps.log_answers('a')
+      @resps.log_answers('a')
+      @resps.log_answers('a')
+      @resps.log_answers('a')
+
+      assert 1, @resps.yeses
+    end
+
+    it 'counts a solo group with a single answer' do
+      @resps.log_answers('b')
+
+      assert 1, @resps.yeses
+    end
   end
 end
