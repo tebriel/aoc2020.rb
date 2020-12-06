e855045591ac05469c85bd4e54a332bbfc434c94
Chris Moultrie <tebriel@github.com>
2020-12-05 23:13:46 -0500

    start day 06

---
+++ lib/day06/main.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+
+if $PROGRAM_NAME == __FILE__
+  lines = []
+  File.foreach('inputs/day06.txt') do |line|
+    lines.append(line.chomp)
+  end
+end
---
+++ test/day06/main_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../../lib/day06/main'
+
+describe '#' do
+  it 'blank test' do
+    assert true
+  end
+end
